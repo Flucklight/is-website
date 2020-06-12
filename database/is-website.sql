33e708dce1680a08ae1673f8a26b3b3ca7c88eee
MauricioMiramontes <mmiramontesr@gmail.com>
2020-06-11 20:30:37 -0500

    Se agregan peliculas y posters

--- database/is-website.sql
+++ database/is-website.sql
@@ -40,15 +40,22 @@ VALUES
 ('Sonic La Película (2019)', 'Accion y Aventura, Animación', 'Jeff Fowler', '1 h 40 min', 'Ingles',
 'Sonic, el descarado erizo azul basado en la famosa serie de videojuegos de Sega, vivirá aventuras y desventuras cuando conoce a su amigo humano y policía, Tom Wachowski (James Marsden).',
 '2020-02-14', 2),
-('Avengers: Endgame (2019)', 'Accion', 'Joe Russo y Anthony Russo', '3 h 2 min', 'Ingles', 
+('Avengers Endgame (2019)', 'Accion', 'Joe Russo y Anthony Russo', '3 h 2 min', 'Ingles', 
 'Tras los sucesos de "Vengadores: Infinity War", los superhéroes que sobrevivieron a Thanos se reunen para poner en práctica un plan definitivo que podría acabar con el villano definitivamente.',
 '2020-04-22', 3),
 ('Guasón (2019)', 'Drama, Suspenso', 'Todd Phillips', '2 h 2 min', 'Ingles', 
 'Película centrada en el Joker, el archienemigo de Batman, que pondrá foco en cómo se convirtió en un villano y qué le llevó a ello. ', 
 '2019-10-04', 4),
-('Justice League Dark: Apokolips War (2020)', 'Animación', 'Matt Peters y Christina Sotta', '1 h 30 min', 'Ingles', 
+('Justice League Dark Apokolips War (2020)', 'Animación', 'Matt Peters y Christina Sotta', '1 h 30 min', 'Ingles', 
 'Culminando seis años de una aventura que comenzó con el lanzamiento de Justice League: War en 2014, Justice League Dark: Apokolips War presenta un planeta Tierra totalmente devastado por el tirano intergaláctico Darkseid en el que los superhéroes apenas han podido hacer frente a la guerra.', 
-'2020-05-05', 5);
+'2020-05-05', 5),
+('Batman vs Superman El Origen de la Justicia (2016)', 'Accion', 'Zack Snyder', '2 h 35 min', 'Ingles',
+'Temiendo las acciones desenfrenadas de un superhéroe con poderes similares a los de un Dios, el formidable y poderoso vigilante de Ciudad Gótica se convierte en salvador moderno y el más aclamado de Metropolis, mientras el mundo parece abrumarse por saber a qué tipo de héroe realmente necesita.',
+'2016-03-24', 6),
+('¡Huye! (2017)', 'Horror', 'Jordan Peele', '1 h 43 min', 'Ingles',
+'El actor y comediante Jordan Peele debuta como director con la película Get Out, una cinta de horror que se centra en la visita que un joven afromericano hará a la familia caucásica de su novia. Sin embargo, no todo es lo que parece y su estadía tomará un giro siniestro.',
+'2017-05-26', 7);
+
 
 
 --
